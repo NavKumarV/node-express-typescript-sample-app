dc65b2a64b65afd6c9a8549cc6a25eb2fca85b03
Naveen Kumar V <40155185+NavKumarV@users.noreply.github.com>
2021-07-13 10:46:18 +0530

    adding cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.get('/', (req, res) => res.send('INDEX'));
 
